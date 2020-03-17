403d82d5cb26f156119478f26322551c909a855e
effective-cyborg-8832 <jorgeguilln@gmail.com>
2020-03-17 14:51:47 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,3 +1,7 @@
 def introduction(name)
   puts "Hi, my name is #{name}."
 end
+
+def intodcution_with_language (name, language)
+  puts "Hi, my name #{name} and I am learning to program in #{language}"
+end
